8d1fa22dcd40e234e4180520a8cb1ebc5ae634cc
KMV <9117479@gmail.com>
2022-08-01 22:49:57 +0300

    Start 1b

--- app/src/main/java/com/kmv/jetpackcomposeedu/MainActivity.kt
+++ app/src/main/java/com/kmv/jetpackcomposeedu/MainActivity.kt
@@ -3,12 +3,16 @@ package com.kmv.jetpackcomposeedu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.kmv.jetpackcomposeedu.ui.theme.JetpackComposeEduTheme
 
@@ -16,13 +20,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            /*NewText()*/
+            Column {
+                Text(text = "Test! Col-1")
+                Text(text = "Test! Col-22")
+                Text(text = "Test! Col-333")
+            }
+            Row(
+                modifier = Modifier.background(Color.LightGray).fillMaxSize()
+            ) {
+                Text(text = "Test! Row-1")
+                Text(text = "Test! Row-22")
+                Text(text = "Test! Row-333")
+            }
         }
     }
 }
 
+/*
 @Preview (showBackground = true)
 @Composable
 fun NewText(){
     Text(text = "Test! test")
-}
+}*/
